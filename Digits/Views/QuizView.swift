9a5971be40db3b14a40a9893b5c5b81736a69969
mcmckinley <mcmckinley@csuchico.edu>
2024-01-23 13:47:13 -0800

    Phone number entry for quiz

--- Digits/Views/QuizView.swift
+++ Digits/Views/QuizView.swift
@@ -6,19 +6,41 @@
 //
 
 import SwiftUI
+import Combine
 
 struct QuizView: View {
     let contacts: [Contact]
     
-    @State var enteredNum: String = ""
-    
+    @State var phoneNumber: String = ""
+    @FocusState private var isFocused: Bool
+
     var body: some View {
         
-        /*
+        
         let testContact = contacts[0]
         
-        TextField("Enter \(testContact.name)'s number:", text: $enteredNum)
-                    .keyboardType(.numberPad)
+        VStack {
+            Text(phoneNumber)
+                .font(.system(size: 24))
+
+            TextField("Enter \(testContact.name)'s number:", text: $phoneNumber)
+                .keyboardType(.numberPad)
+                .font(.system(size: 24))
+                .focused($isFocused)
+                .padding([.leading], 3)
+                .onChange(of: phoneNumber, initial: false) { newPhoneNumber, what in
+                    if newPhoneNumber.count == 9 {
+                    // Perform your desired action when 10 characters are entered
+                    print("Phone number entered: \(newPhoneNumber)")
+                    // Disable the keypad or perform any other action here
+                    isFocused = false
+                }
+            }
+        }
+        .onAppear {
+            isFocused = true
+        }
+        /*
                     .onReceive(Just(enteredNum)) { newValue in
                         let filtered = newValue.filter { "0123456789".contains($0) }
                         if filtered != newValue {
@@ -26,7 +48,7 @@ struct QuizView: View {
                         }
                     }
          */
-        Text("Why hello")
+        
         
     }
 }
