f81efe6fee51d7ce6577fcbf6a330c542965ac71
mcmckinley <mcmckinley@csuchico.edu>
2024-01-24 10:28:11 -0800

    Dynamic number display as user enters answer

--- Digits/Views/QuizView.swift
+++ Digits/Views/QuizView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct QuizView: View {
     let contacts: [Contact]
@@ -16,39 +17,34 @@ struct QuizView: View {
     @State var responses: [String] = []
     @State var index: Int = 0
     
-    @State var userEntryArray: [Character] = [" "," "," "," "," "," "," "," "," "," "]
+    @State private var userEntryArray: [String] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
     
     var body: some View {
         // MAYBE we instead type it all to an invisible text box, and have a seperate function which displays it to each box, rather than split it up and divide the FocusState
-        HStack {
-            Text(String(userEntryArray[0]))
-            Text(String(userEntryArray[1]))
-            Text(String(userEntryArray[2]))
-            Text(String(userEntryArray[3]))
-            Text(String(userEntryArray[4]))
-            Text(String(userEntryArray[5]))
-            Text(String(userEntryArray[6]))
-            Text(String(userEntryArray[7]))
-            Text(String(userEntryArray[8]))
-            Text(String(userEntryArray[9]))
-        }
+        
+        
+        
         let testContact = contacts[0]
         ZStack {
-            TextField("Enter \(testContact.name)'s number:", text: $phoneNumber)
+            HStack {
+                ForEach(userEntryArray, id: \.self) { char in
+                    Text(char)
+                        .font(.system(size: 24))
+                }
+                Spacer()
+                
+            }
+            TextField("", text: $phoneNumber)
                 .keyboardType(.numberPad)
+                .opacity(0)
                 .font(.system(size: 24))
                 .focused($isFocused)
                 .padding([.leading], 3)
                 .onChange(of: phoneNumber, initial: false) { previousNumber, currentNumber in
-                    // If the user entered a digit...
-                    if currentNumber > previousNumber {
-                        userEntryArray[index]
-                        index+=1
-                        
-                    }
-                    // If the user removed a digit...
-                    else {
-                        index-=1
+                    
+                    userEntryArray = phoneNumber.map{String($0)}
+                    for entry in userEntryArray {
+                        print("Entry:  \(entry)")
                     }
                     
                     // If the phone number is complete...
